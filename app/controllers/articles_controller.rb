16ee131d2754486aec7b58343d6621ebf5d5e040
blacksyan <ilham.adhy@gmail.com>
2018-11-15 16:29:08 +0700

    Update to style rubocop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,62 +1,60 @@
+# frozen_string_literal: true
+
+# define article controller
 class ArticlesController < ApplicationController
-    
-    # define index method to show all article
-    def index
-        @articles = Article.all
+  # define index method to show all article
+  def index
+    @articles = Article.all
+  end
+
+  # define show method to show article
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # define new method
+  def new
+    @article = Article.new
+  end
+
+  # define update method
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # define create method
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
-    # define show method to show article
-    def show
-        @article = Article.find(params[:id])
+  # define update method to save from edit
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
-
-    # define new method
-    def new
-        @article = Article.new
-    end
-
-    # define update method
-    def edit
-        @article = Article.find(params[:id])
-
-    end
-
-    # define create method
-    def create
-        @article = Article.new(article_params)
-        
-        if @article.save
-        redirect_to @article
-        else
-            render 'new'
-        end            
-    end
-
-    # define update method to save from edit
-    def update
-        @article = Article.find(params[:id])
-       
-        if @article.update(article_params)
-          redirect_to @article
-        else
-          render 'edit'
-        end
-      end
-
-    # define method to delete row
-    def destroy
+  # define method to delete row
+  def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    
     redirect_to articles_path
-    end
+  end
 
-    private
-    # to whitelist our controller parameter
-    def article_params
-        params.require(:article).permit(:title, :text)
-    end
+  private
 
-
+  # to whitelist our controller parameter
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# define article model
 class Article < ApplicationRecord
-    validates :title, presence: true,
-                    length: { minimum: 5}
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
